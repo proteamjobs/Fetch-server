b811d535306a238ecc9106c07dbd2285e34be7cf
박세현 <ongsim9@gmail.com>
2019-05-30 20:35:09 +0900

    basic routes setting complete

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+const users = require("./users");
+const orders = require("./orders");
+const travels = require("./travels");
+const directmessages = require("./directmessages");
+const reviews = require("./reviews");
+const notice = require("./notice");
+
+module.exports = { users, orders, travels, directmessages, reviews, notice };
