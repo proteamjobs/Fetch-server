1a51614665974cc7c069d2739beb583f26afe8e0
박세현 <ongsim9@gmail.com>
2019-06-04 15:35:57 +0900

    Fix DB schema setting

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ let db = require("./models/index.js");
 
 db.sequelize
   .sync()
-  //   .sync({ force: true })
+  // .sync({ force: true })
   .then(() => {
     console.log(" DB Connect!");
   })
