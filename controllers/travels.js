a0adac8f39e79ea3028c3cce4076782703c51bc6
박세현 <ongsim9@gmail.com>
2019-06-07 11:33:48 +0900

    feature/travels -travels/list basic complete

--- controllers/travels.js
+++ controllers/travels.js
@@ -1,7 +1,47 @@
+const db = require("../models");
+
 module.exports = {
   list: {
-    get: (req, res) => {
-      res.status(201).send("GET /travels/list OK!");
+    get: async (req, res) => {
+      try {
+        let travel = {
+          travelList: []
+        };
+        let list = await db.travels.findAll();
+        list.map(item => {
+          let temp = {
+            travel_id: item.dataValues._id,
+            destination: item.dataValues.destination,
+            arrivingDate: item.dataValues.arrivalDate,
+            traveler_id: item.dataValues.traveler_id
+          };
+          travel.travelList.push(temp);
+        });
+
+        for (let i = 0; i < travel.travelList.length; i++) {
+          let name = await db.users.findOne({
+            where: {
+              _id: travel.travelList[i].traveler_id
+            },
+            attributes: ["name"]
+          });
+          // console.log(name.dataValues.name);
+          travel.travelList[i].travelerName = name.dataValues.name;
+        }
+
+        travel.travelList.reverse();
+
+        if (req.query.max) {
+          travel.travelList = travel.travelList.splice(0, req.query.max);
+          res.status(200).send(travel);
+        } else {
+          res.status(200).send(travel);
+        }
+      } catch (err) {
+        console.log("ERROR ::: ", err);
+        res.status(400).send(err);
+      }
+      // res.status(201).send("GET /travels/list OK!");
     }
   },
   detail: {
