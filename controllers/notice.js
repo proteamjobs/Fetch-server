a977df0247483fde31ea0a9caa94957abca6108c
박세현 <ongsim9@gmail.com>
2019-06-12 09:59:06 +0900

    feature/travels && notice basic complete

--- controllers/notice.js
+++ controllers/notice.js
@@ -1,7 +1,22 @@
+const db = require("../models");
+
 module.exports = {
   detail: {
     get: (req, res) => {
       res.status(201).send("GET /notice/detail OK!");
+    }
+  },
+  create: {
+    post: (req, res) => {
+      console.log(req.body);
+      db.notices
+        .create({
+          title: req.body.title,
+          message: req.body.message
+        })
+        .then(() => {
+          res.status(201).send("POST /notice/create OK!");
+        });
     }
   }
 };
